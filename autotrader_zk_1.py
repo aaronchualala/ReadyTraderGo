8cb4d7d0c654d66bdb942afa22508b9c2680799a
lowzk <lowzhekai69@gmail.com>
2023-03-11 01:22:48 +0800

    zk/model1

--- autotrader_zk_1.py
+++ autotrader_zk_1.py
@@ -91,31 +91,32 @@ class AutoTrader(BaseAutoTrader):
             self.cur_ETF_bid = bid_prices[0]
 
         if instrument == Instrument.FUTURE:
-            self.cur_FUT_ask = ask_prices[0]
-            self.cur_FUT_bid = bid_prices[0]
-            
-            price_adjustment = - (self.position // LOT_SIZE) * TICK_SIZE_IN_CENTS
-            new_bid_price = bid_prices[0] + price_adjustment if bid_prices[0] != 0 else 0
-            new_ask_price = ask_prices[0] + price_adjustment if ask_prices[0] != 0 else 0
+            self.cur_FUT_ask = new_ask_price = ask_prices[0]
+            self.cur_FUT_bid = new_bid_price = bid_prices[0]
 
-            if self.bid_id != 0 and new_bid_price not in (self.bid_price, 0):
-                self.send_cancel_order(self.bid_id)
-                self.bid_id = 0
-            if self.ask_id != 0 and new_ask_price not in (self.ask_price, 0):
-                self.send_cancel_order(self.ask_id)
-                self.ask_id = 0
+            '''Decide whether we want to place an order based on the ETF price
+            For now, if the FUT_price * 1.05 > ETF_price, we place an bid for the ETF
+            If the FUT_price * 0.95 < ETF_price, we place a ask for the ETF'''
+            if (self.cur_FUT_bid > self.cur_ETF_bid * 1.05):
+                if self.bid_id != 0 and new_bid_price not in (self.bid_price, 0):
+                    self.send_cancel_order(self.bid_id)
+                    self.bid_id = 0
+                if self.bid_id == 0 and new_bid_price != 0 and self.position < POSITION_LIMIT:
+                    self.bid_id = next(self.order_ids)
+                    self.bid_price = new_bid_price
+                    self.send_insert_order(self.bid_id, Side.BUY, new_bid_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
+                    self.bids.add(self.bid_id)
 
-            if self.bid_id == 0 and new_bid_price != 0 and self.position < POSITION_LIMIT:
-                self.bid_id = next(self.order_ids)
-                self.bid_price = new_bid_price
-                self.send_insert_order(self.bid_id, Side.BUY, new_bid_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
-                self.bids.add(self.bid_id)
+            if (self.cur_FUT_ask < self.cur_ETF_ask * 0.95):
+                if self.ask_id != 0 and new_ask_price not in (self.ask_price, 0):
+                    self.send_cancel_order(self.ask_id)
+                    self.ask_id = 0
+                if self.ask_id == 0 and new_ask_price != 0 and self.position > -POSITION_LIMIT:
+                    self.ask_id = next(self.order_ids)
+                    self.ask_price = new_ask_price
+                    self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
+                    self.asks.add(self.ask_id)
 
-            if self.ask_id == 0 and new_ask_price != 0 and self.position > -POSITION_LIMIT:
-                self.ask_id = next(self.order_ids)
-                self.ask_price = new_ask_price
-                self.send_insert_order(self.ask_id, Side.SELL, new_ask_price, LOT_SIZE, Lifespan.GOOD_FOR_DAY)
-                self.asks.add(self.ask_id)
 
     def on_order_filled_message(self, client_order_id: int, price: int, volume: int) -> None:
         """Called when one of your orders is filled, partially or fully.
